c750d27af4ebbc2d0043af6e50393e82a71ed2f9
moejetz <moe@moes-mac.local>
2015-09-28 12:49:01 +0300

    fix: ip observer script now in production state

--- ip_management/client_scripts/say_hello.sh
+++ ip_management/client_scripts/say_hello.sh
@@ -1,34 +1,29 @@
 #!/bin/sh
 
-#get initial data
-ip=`ifconfig br0 | awk '/inet addr/{printf substr($2,6)}'`
+#initialize variables
+ip=""
 mac=`ifconfig br0 | egrep -o '([[:xdigit:]]{2}[:]){5}[[:xdigit:]]{2}'`
 
 
 while true; do
 
   #get current assigned ip
-  tmp_ip=`ifconfig br0 | awk '/inet addr/{printf substr($2,6)}'`
+  tmp_ip=`ifconfig br0 | awk '/inet addr/{printf substr($2,6)}'` 
 
   #check for changes
-  if [ "$ip" = "$tmp_ip" ]; then
-    echo "ip did not change!"
-
-    #FOR TESTING - REMOVE FOR PRODUCTION!
-    ip="false_ip"
-
-  else
-
+  if [ "$ip" != "$tmp_ip" ]; then 
+    
     #ip has changed, inform server
     echo "ip changed!"
-    ip="$tmp_ip"
-
+    ip="$tmp_ip" 
+    
     data="mac=$mac&ip=$ip"
     curl --data "$data" http://130.233.193.135:9999/plugs
-
+  
   fi;
-
+  
   #sleep a short while
   sleep 2
+
 
 done
