d06e476e857bcefba420a10f36c90e56431ec5cd
moejetz <moe@moes-mac.local>
2015-09-30 10:19:35 +0300

    fix: tlhAgent runs forever, tries to re-establish lost server connections automatically

--- agents/tlhAgent.py
+++ agents/tlhAgent.py
@@ -25,6 +25,87 @@ omi_node_ip = "130.233.193.79"
 omi_node_external_agent_port = 8181
 sleepTime = 5
 
+
+#methods
+
+def establishConnection():
+    while (True):
+        try:
+            #open connection to o-mi node
+            global connection
+            connection = create_connection((omi_node_ip, omi_node_external_agent_port))
+            break
+        except Exception as e:
+            print (e)
+            time.sleep(sleepTime)
+
+
+def run():
+    while True:
+
+        try:
+
+            #get current values
+            humidity    = io.open('/mnt/1wire/26.DD4DCB010000/humidity', 'r')   .read().strip()
+            temperature = io.open('/mnt/1wire/26.595D45010000/temperature', 'r').read().strip()
+            lightVoltage= float(io.open('/mnt/1wire/26.595D45010000/VAD', 'r')  .read().strip())
+
+            lightLux    = 12779 * lightVoltage - 121.89
+            lightLux = round(lightLux, 2)
+            if(lightLux<0):
+                lightLux=0
+            lightLux=str(lightLux)
+
+            odf_message = b'''
+                <Objects xmlns="odf.xsd">
+                    <Object>
+                        <id>CS Building</id>
+                        <Object>
+                            <id>Room B126</id>
+                            <InfoItem name="temperature">
+                                <value>''' +\
+                                    bytes(temperature, "utf-8") +\
+                                b'''</value>
+                            </InfoItem>
+                            <InfoItem name="light">
+                                <value>''' +\
+                                    bytes(lightLux, "utf-8") +\
+                                b'''</value>
+                            </InfoItem>
+                            <InfoItem name="humidity">
+                                <value>''' +\
+                                    bytes(humidity, "utf-8") +\
+                                b'''</value>
+                            </InfoItem>
+                        </Object>
+                    </Object>
+                </Objects>'''
+
+            #send new sensor data
+            connection.sendall(odf_message)
+
+
+            #verbose output
+            if(verbose):
+                print ('Temperature:\t\t', temperature)
+                print ('Light:\t\t\t', lightLux)
+                print ('Humidity:\t\t', humidity)
+                print ('==================================')
+
+
+        except Exception as e:
+            print (e)
+            establishConnection()
+
+
+        finally:
+            # wait for 15 seconds
+            time.sleep(sleepTime)
+
+
+
+
+
 #check arguments
 verbose=False
 startMessage = '\r\n=================\r\n[Starting agent]\r\n================='
@@ -39,61 +120,6 @@ if(len(sys.argv)>1):
 print (startMessage)
 
 
-#open connection to o-mi node
-connection = create_connection((omi_node_ip, omi_node_external_agent_port))
-
-
-try:
-    while True:
-
-        #get current values
-        humidity    = io.open('/mnt/1wire/26.DD4DCB010000/humidity', 'r')   .read().strip()
-        temperature = io.open('/mnt/1wire/26.595D45010000/temperature', 'r').read().strip()
-        lightVoltage= float(io.open('/mnt/1wire/26.595D45010000/VAD', 'r')	.read().strip())
-
-        lightLux    = 12779 * lightVoltage - 121.89
-        lightLux = round(lightLux, 2)
-        if(lightLux<0):
-            lightLux=0
-        lightLux=str(lightLux)
-
-        #verbose output
-        if(verbose):
-            print ('Temperature:\t\t', temperature)
-            print ('Light:\t\t\t', lightLux)
-            print ('Humidity:\t\t', humidity)
-            print ('==================================')
-
-        odf_message = b'''
-            <Objects xmlns="odf.xsd">
-                <Object>
-                    <id>CS Building</id>
-                    <Object>
-                        <id>Room B126</id>
-                        <InfoItem name="temperature">
-                            <value>''' +\
-                            	bytes(temperature, "utf-8") +\
-                            b'''</value>
-                        </InfoItem>
-                        <InfoItem name="light">
-                            <value>''' +\
-                            	bytes(lightLux, "utf-8") +\
-                            b'''</value>
-                        </InfoItem>
-                        <InfoItem name="humidity">
-                            <value>''' +\
-                                bytes(humidity, "utf-8") +\
-                            b'''</value>
-                        </InfoItem>
-                    </Object>
-                </Object>
-            </Objects>'''
-
-        #send new data and wait for 15 seconds
-        connection.sendall(odf_message)
-        time.sleep(sleepTime)
-
-except Exception as e:
-    print (e)
-    print ('Shutting down agent...')
-    exit(0)
+#start actual work
+establishConnection()
+run()
